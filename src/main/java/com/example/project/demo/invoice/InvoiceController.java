fa4247606864ebebd0ea383cb71b92432a34dde6
rafiulabeerkhan <rafiulabeerkhan296@gmail.com>
2024-03-18 16:26:23 +0600

    Jasper Added

--- src/main/java/com/example/project/demo/invoice/InvoiceController.java
+++ src/main/java/com/example/project/demo/invoice/InvoiceController.java
@@ -2,10 +2,20 @@ package com.example.project.demo.invoice;
 
 import com.example.project.demo.EmployeeLeave.LeaveEntity;
 import com.example.project.demo.EmployeeLeave.LeaveService;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/invoice")
@@ -13,7 +23,38 @@ import java.util.List;
 public class InvoiceController {
     @Autowired
     public InvoiceService service;
+    @Autowired
+    public InvoiceRepository repository;
 
+
+
+    @GetMapping(value = "/pdf/{id}", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<byte[]> downloadInvoice( @PathVariable("id") Long id) throws JRException, IOException {
+
+        JRBeanCollectionDataSource beanCollectionDataSource = new JRBeanCollectionDataSource(
+                List.of(repository.findById(id).orElse(new InvoiceEntity()))
+                , false);
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("total", "7000");
+
+        JasperReport compileReport = JasperCompileManager
+                .compileReport(new FileInputStream("src/main/resources/Invoice.jrxml"));
+
+        JasperPrint jasperPrint = JasperFillManager.fillReport(compileReport, parameters, beanCollectionDataSource);
+
+        // JasperExportManager.exportReportToPdfFile(jasperPrint,
+        // System.currentTimeMillis() + ".pdf");
+
+        byte data[] = JasperExportManager.exportReportToPdf(jasperPrint);
+
+        System.err.println(data);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "inline; filename=citiesreport.pdf");
+
+        return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF).body(data);
+    }
     @GetMapping("/getAll")
     public List<InvoiceEntity> getAllInvoice(){
         return service.getAllInvoice();
